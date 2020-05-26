a64f1cc7b64d0a30c3b433d096b54fde8d361eba
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2020-05-26 19:31:12 +0530

    Initial project

---
+++ components/Card.jsx
@@ -0,0 +1,28 @@
+
+
+const Card = () => {
+    return (
+        <div className="card-main">
+            <div className="card-c-1">
+                <h4>Dashboard for an IOT Application</h4>
+                <p className="card-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam,
+                    totam voluptatum, vel nam a beatae excepturi minima,
+                    ducimus aliquam perferendis ut! Ea quae impedit quam.
+                    Temporibus corrupti aut earum!
+            </p>
+            </div>
+            <div className="card-c-2">
+                <div className="">
+                    <span>Icon</span>
+                    <p>John Doe</p>
+                </div>
+                <div>
+                    <p>React</p>
+                    <p>Nodejs</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Card
