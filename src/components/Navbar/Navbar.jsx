6172b70e963dcc40a8afe257fd05633c90b78081
yogesh-hackx <yogesh.hackx@gmail.com>
2020-06-03 15:28:31 +0530

    navbar styles update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,31 @@
-import styles from './Navbar.module.css'
-import { GiHamburgerMenu } from 'react-icons/gi'
+import { useContext } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { LayoutContext } from '../../context/LayoutContext';
+import styles from './Navbar.module.css';
 
 const Navbar = () => {
-    const isAuthenticated = true
+    const { sideBar, toggleSideBar } = useContext(LayoutContext);
+    const isAuthenticated = true;
     return (
         <div>
             <ul className={styles.navContainer}>
-                {isAuthenticated ? <GiHamburgerMenu /> : <><li className={styles.navElements}>Logo/Name</li>
-                    <li className={`${styles.navFloat} ${styles.navli}`}>Login</li>
-                    <li className={styles.navFloat}>SignUp</li></>}
+                {isAuthenticated ? (
+                    <GiHamburgerMenu
+                        onClick={() => toggleSideBar(sideBar)}
+                        size="1.35em"
+                        className={styles.hamburger}
+                    />
+                ) : (
+                    <>
+                        <li className={styles.navElements}>Logo/Name</li>
+                        <li className={`${styles.navFloat} ${styles.navli}`}>
+                            Login
+                        </li>
+                        <li className={styles.navFloat}>SignUp</li>
+                    </>
+                )}
             </ul>
         </div>
-    )
-}
-export default Navbar
+    );
+};
+export default Navbar;
