cb1f96b47b636727848f5dacff846dd626d7a61d
yogesh-hackx <yogesh.hackx@gmail.com>
2020-06-03 15:30:56 +0530

    controls for sidebar expand-shrink

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,29 +1,38 @@
-import styles from "./Sidebar.module.css";
-import { IoMdAddCircle } from "react-icons/io";
-import { AiFillHome, AiFillSetting } from "react-icons/ai";
-import { MdBook } from "react-icons/md";
-import { useState } from "react";
+import { useContext } from 'react';
+import styles from './Sidebar.module.css';
+import { IoMdAddCircle } from 'react-icons/io';
+import { AiFillHome, AiFillSetting } from 'react-icons/ai';
+import { MdBook } from 'react-icons/md';
+import { useState } from 'react';
+import { LayoutContext } from '../../context/LayoutContext';
 
 const Sidebar = () => {
-    const active = useState('home')
+    const active = useState('home');
+    const { sideBar, toggleSideBar } = useContext(LayoutContext);
+    let sideBarStyle =
+        sideBar === 'expanded' ? styles.expanded : styles.shrinked;
     return (
-        <div className={styles.sbarMain}>
-            <div className={styles.sbarh1}>NONAME</div>
-            <button className={styles.sbarAddPost}>
-                <IoMdAddCircle size="1.35em" />
-                <span className={styles.sbNewButLabel}>New Post</span>
-            </button>
-            <div className={styles.sbHome}>
-                <AiFillHome size="1.2em" />
-                <span className={styles.sbItemName}>Home</span>
-            </div>
-            <div className={styles.sbPost}>
-                <MdBook size="1.2em" />
-                <span className={styles.sbItemName}>My Post</span>
-            </div>
-            <div className={styles.sbSettings}>
-                <AiFillSetting size="1.2em" />
-                <span className={styles.sbItemName}>Settings</span>
+        <div className={styles.sidebarLeft} onMou>
+            <div className={sideBarStyle}>
+                <div className={styles.sbarh1}>NONAME</div>
+                <button className={styles.sbarAddPost}>
+                    <IoMdAddCircle size="1.35em" />
+                    <span className={styles.sbNewButLabel}>New Post</span>
+                </button>
+                <div
+                    className={`${styles.sbHome} ${styles.sideTab} ${styles.selectedTab}`}
+                >
+                    <AiFillHome size="1.2em" />
+                    <span className={styles.sbItemName}>Home</span>
+                </div>
+                <div className={`${styles.sideTab}`}>
+                    <MdBook size="1.2em" />
+                    <span className={styles.sbItemName}>My Post</span>
+                </div>
+                <div className={`${styles.sideTab}`}>
+                    <AiFillSetting size="1.2em" />
+                    <span className={styles.sbItemName}>Settings</span>
+                </div>
             </div>
         </div>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
-import Cards from "../components/Cards";
-import Navbar from '../components/Navbar/Navbar'
-import Sidebar from '../components/Sidebar/Sidebar'
-
+import Cards from '../components/Cards';
+import Navbar from '../components/Navbar/Navbar';
+import Sidebar from '../components/Sidebar/Sidebar';
 
 export default function Home() {
     return (
